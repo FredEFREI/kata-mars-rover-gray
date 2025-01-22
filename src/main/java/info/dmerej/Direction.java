920eb7e60f9303236093ac0034b62c37f5afe4f5
FredEFREI <frederic.casier@efrei.net>
2025-01-22 16:38:57 +0100

    Refactorization of turnRight and turnLeft

--- src/main/java/info/dmerej/Direction.java
+++ src/main/java/info/dmerej/Direction.java
@@ -1,5 +1,13 @@
 package info.dmerej;
 
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST;
+    NORTH(0),
+    EAST(1),
+    SOUTH(2),
+    WEST(3);
+
+    public int value;
+    Direction(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/info/dmerej/Rover.java
+++ src/main/java/info/dmerej/Rover.java
@@ -46,37 +46,12 @@ public class Rover {
     }
 
     public void turnLeft(){
-        switch(direction){
-            case NORTH:
-                direction = Direction.WEST;
-                break;
-            case SOUTH:
-                direction = Direction.EAST;
-                break;
-            case EAST:
-                direction = Direction.NORTH;
-                break;
-            case WEST:
-                direction = Direction.SOUTH;
-                break;
-        }
+        int newindex = (direction.ordinal() - 1 + 4) % 4;
+        direction = Direction.values()[newindex];
     }
 
     public void turnRight(){
-        switch(direction){
-            case NORTH:
-                direction = Direction.EAST;
-                break;
-            case SOUTH:
-                direction = Direction.WEST;
-                break;
-            case EAST:
-                direction = Direction.SOUTH;
-                break;
-            case WEST:
-                direction = Direction.NORTH;
-                break;
-
-        }
+        int newindex = (direction.ordinal() + 1 + 4) % 4;
+        direction = Direction.values()[newindex];
     }
 }
