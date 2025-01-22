6a8cb55c9241ffd200bc01c44aa31197c59ecf76
FredEFREI <frederic.casier@efrei.net>
2025-01-22 15:27:40 +0100

    Forward test working

--- src/test/java/info/dmerej/RoverTest.java
+++ src/test/java/info/dmerej/RoverTest.java
@@ -19,6 +19,36 @@ public class RoverTest {
     }
 
     @Test
+    void move_forward_facing_east() {
+        int x = 4;
+        int y = 2;
+        Rover rover = new Rover(x, y, Direction.EAST);
+        rover.moveForward();
+        assertEquals(5, rover.x);
+        assertEquals(2, rover.y);
+    }
+
+    @Test
+    void move_forward_facing_west() {
+        int x = 4;
+        int y = 2;
+        Rover rover = new Rover(x, y, Direction.WEST);
+        rover.moveForward();
+        assertEquals(3, rover.x);
+        assertEquals(2, rover.y);
+    }
+
+    @Test
+    void move_forward_facing_south() {
+        int x = 4;
+        int y = 2;
+        Rover rover = new Rover(x, y, Direction.SOUTH);
+        rover.moveForward();
+        assertEquals(4, rover.x);
+        assertEquals(1, rover.y);
+    }
+
+    @Test
     void move_backwards_facing_north() {
         int x = 4;
         int y = 2;
@@ -28,13 +58,5 @@ public class RoverTest {
         assertEquals(1, rover.y);
     }
 
-    @Test
-    void move_forward_facing_east() {
-        int x = 4;
-        int y = 2;
-        Rover rover = new Rover(x, y, Direction.EAST);
-        rover.moveForward();
-        assertEquals(5, rover.x);
-        assertEquals(2, rover.y);
-    }
+
 }
